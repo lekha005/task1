ba6110f42d83d3def925bf5e9b54a92d5d27c712
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-30 06:13:43 +0000

    Update Al.java

--- Al.java
+++ Al.java
@@ -4,14 +4,22 @@ class Al
     public static void main(String hj[])
     {
         Scanner in=new Scanner(System.in);
-       String n=in.next();
+       String n=in.next().toLowerCase();
+       char a=n.charAt(0);
+      if((int)a>=94 &&(int)a<=122)
+       {
     if(n.contains("a") || n.contains("e") || n.contains("i") || n.contains("o") || n.contains("u"))
     {
         System.out.print("Vowel");
     }
     else
     {
-        System.out.print("Invalid");
+        System.out.print("Consonant");
     }
+       }
+       else
+       {
+           System.out.print("invalid");
+       }  
     }
 }
