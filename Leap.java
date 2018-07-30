16089fa7cc60ff416927c118be05725c0e26c747
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-30 06:34:55 +0000

    Create Leap.java

---
+++ Leap.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Leap
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+         int n=in.nextInt();
+         if(n%4==0)
+      System.out.print("yes");
+      else
+      System.out.print("no");
+    }
+}
