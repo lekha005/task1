57341fc0d2b0f4b24aaa81801bc52a3932973d5e
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-30 04:26:33 +0000

    Create Odev.java

---
+++ Odev.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class add
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+       int n=in.nextInt();
+    if(n%2==0)
+    {
+        System.out.print("Even");
+    }
+    else
+    System.out.print("Odd");
+    }
+}
