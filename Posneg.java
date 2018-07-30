90486d749489fcfa3b1427f390869a7e5eb18006
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-30 04:21:15 +0000

    Create Posneg.java

---
+++ Posneg.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Posneg
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+       int n=in.nextInt();
+    if(n>=0)
+    {
+        System.out.print("Positive");
+    }
+    else
+    System.out.print("Negative");
+    }
+}
