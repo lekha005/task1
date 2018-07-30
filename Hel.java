84ab31531431e57936dc2030eca34c202a786603
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-30 06:39:14 +0000

    Create Hel.java

---
+++ Hel.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Hel
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+         int n=in.nextInt();
+         for(int i=0;i<n;i++)
+         {
+      System.out.println("Hello");
+         }
+    }
+}
