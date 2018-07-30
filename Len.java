76234c6be298abd0656ec7b7ad7b52f5fc74cb50
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-30 06:55:28 +0000

    Create Len.java

---
+++ Len.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Len
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+         int n=in.nextInt();
+         String a=String.valueOf(n);
+        System.out.print(a.length());
+    }
+}
