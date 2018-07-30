d5f52014d805f23b6c596a114a1d6927fadb77d0
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-30 04:35:30 +0000

    Update Odev.java

--- Odev.java
+++ Odev.java
@@ -5,11 +5,18 @@ class add
     {
         Scanner in=new Scanner(System.in);
        int n=in.nextInt();
+if(n<0)
+{
+    System.out.print("invalid");
+                     }
+        else
+        {
     if(n%2==0)
     {
         System.out.print("Even");
     }
     else
     System.out.print("Odd");
+    }
     }
 }
