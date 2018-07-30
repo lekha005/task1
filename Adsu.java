7c328a9d77ff936dce848108dcf937c9d2f6b138
lekha005 <41885177+lekha005@users.noreply.github.com>
2018-07-30 06:41:41 +0000

    Create Adsu.java

---
+++ Adsu.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Adsu
+{
+    public static void main(String hj[])
+    {
+        Scanner in=new Scanner(System.in);
+         int n=in.nextInt();
+         int sum=0;
+         for(int i=1;i<=n;i++)
+         {
+        sum+=i;
+         }
+         System.out.print(sum);
+    }
+}
